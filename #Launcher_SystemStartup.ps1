f4ed55e51c0854ecf57d199e3c048606f76bc5af
wvzxn <zvp.ukr01@gmail.com>
2024-01-09 19:48:31 +0200

    upd

--- #Launcher_SystemStartup.ps1
+++ #Launcher_SystemStartup.ps1
@@ -18,6 +18,8 @@ Launcher $files "^.*(?<!_END|_onlyAC)\.(lnk|exe)$" 500                          
 #Launcher $files "^.*(?<!_END|_onlyAC)\.(ps1|cmd|bat|vbs)$" 250 $itemArgs                # [*.<ps1/cmd/bat/vbs>]
 if ($isCharging) { Launcher $files "^.*_onlyAC\.(lnk|exe)$" 500 }                       # [_onlyAC.<lnk/exe>]
 #if ($isCharging) { Launcher $files "^.*_onlyAC\.(ps1|cmd|bat|vbs)$" 250 $itemArgs }     # [_onlyAC.<ps1/cmd/bat/vbs>]
+
+Start-Sleep -Seconds 5
 Launcher $files "^.*(?<!_onlyAC)_END\.(lnk|exe)$" 500                                   # [_END.<lnk/exe>]
 #Launcher $files "^.*(?<!_onlyAC)_END\.(ps1|cmd|bat|vbs)$" 250 $itemArgs                 # [_END.<ps1/cmd/bat/vbs>]
 if ($isCharging) { Launcher $files "^.*_onlyAC_END\.(lnk|exe)$" 500 }                   # [_onlyAC_END.<lnk/exe>]
