cbd9266ad522275f7de3a403d36ae22dedfe5267
wvzxn <zvp.ukr01@gmail.com>
2024-02-08 09:46:05 +0200

    upd

--- #Launcher_PowerStateChanged.ps1
+++ #Launcher_PowerStateChanged.ps1
@@ -1,10 +1,10 @@
 #  #Launcher_BatteryStatus (PowerShell)
 
 $isCharging = (Get-CIMInstance Win32_Battery).BatteryStatus -ne 1
+Set-Location $PSScriptRoot
 $startStop = if ($isCharging) {"Start"} else {"Stop"}
 $onOff = if ($isCharging) {"On"} else {"Off"}
-Set-Location $PSScriptRoot
 
-& ".\BluetoothControl.ps1" $onOff; Start-Sleep -Milliseconds 250
-& ".\SyncTrayzor.ps1" $startStop; Start-Sleep -Milliseconds 250
-& ".\Spotify.ps1" $startStop -force; Start-Sleep -Milliseconds 250
+& ".\BluetoothControl.ps1" $onOff
+& ".\SyncTrayzor.ps1" $startStop
+& ".\Spotify.ps1" $startStop -force
--- #Launcher_WinStart.ps1
+++ #Launcher_WinStart.ps1
@@ -2,19 +2,14 @@
 
 $isCharging = (Get-CIMInstance Win32_Battery).BatteryStatus -ne 1
 Set-Location $PSScriptRoot
-$files = Get-ChildItem | Where-Object { $_.Name -match "^[^\#].*\.(lnk|exe|ps1|cmd|bat|vbs)$" }
 
-function Launcher ($gciFiles, $regex, $ms)
-{
-    $gciFiles | `
-    Where-Object { $_.Name -match $regex } | `
-    ForEach-Object { & $_.FullName; Start-Sleep -Milliseconds $ms }
-}
+& ".\EQ APO (PhilipsOff + VolDown).lnk"
+& ".\Rainmeter.lnk"
+& ".\Sandboxie.lnk"
+Start-Sleep -Seconds 3
+& ".\AutoDarkMode.lnk"
 
-Launcher $files "^.*(?<!_END|_onlyAC)\.(lnk|exe)$" 500                                  # [*.<lnk/exe>]
-if ($isCharging) { Launcher $files "^.*_onlyAC\.(lnk|exe)$" 500 }                       # [_onlyAC.<lnk/exe>]
-Start-Sleep -Seconds 5
-Launcher $files "^.*(?<!_onlyAC)_END\.(lnk|exe)$" 500                                   # [_END.<lnk/exe>]
+if (!$isCharging) { return }
 
-if ($isCharging) { & ".\SyncTrayzor.ps1" Start }
-if ($isCharging) { & ".\Spotify.ps1" Start }
+& ".\SyncTrayzor.ps1" Start
+& ".\Spotify.ps1" Start
