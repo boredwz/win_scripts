90452ebff5aa1f4ad9b4fca0c334808eb0ea6ced
boredwz <boredwzzzz@gmail.com>
2025-03-05 01:25:30 +0200

    init

---
+++ ps/restart_explorer_restore_foreground.ps1
@@ -0,0 +1,25 @@
+param([Alias("m","min","minimized")][switch]$Minimize)
+
+$fore_url = "https://github.com/boredwz/win_scripts/blob/master/ps/foreground_window.ps1?raw=1"
+$rest_url = "https://github.com/boredwz/win_scripts/blob/master/ps/restart_explorer.ps1?raw=1"
+$fore_exists = Test-Path ".\foreground_window.ps1" -PathType Leaf
+$rest_exists = Test-Path ".\restart_explorer.ps1" -PathType Leaf
+if ($Minimize) {$rest_arg = "-Minimize"}
+
+$activeWindowId = `
+    if ($fore_exists) {
+        (& ".\foreground_window.ps1" -get)
+    } else {
+        Invoke-Expression "&{$(Invoke-WebRequest -useb $fore_url)} -Get"
+    }
+
+'{0} | restart' -f (get-date -f "hh:mm:ss.fff")
+if ($rest_exists) {
+    & ".\restart_explorer.ps1" $rest_arg
+} else {
+    Invoke-Expression "&{$(Invoke-WebRequest -useb $rest_url)} $rest_arg"
+}
+
+Start-Sleep 2
+'{0} | restore foreground window' -f (get-date -f "hh:mm:ss.fff")
+$null = (& ".\foreground_window.ps1" -set $activeWindowId)
