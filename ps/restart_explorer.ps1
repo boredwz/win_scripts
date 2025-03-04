89f991d2fc5df43de23e2fbe88383c991d1875ed
boredwz <boredwzzzz@gmail.com>
2025-03-05 00:18:01 +0200

    Windows 11 edition: - Fixed empty ClassID location; - Increased loading time; - Added Explorer Tab Utility link to the description

--- ps/restart_explorer.ps1
+++ ps/restart_explorer.ps1
@@ -6,33 +6,25 @@ Restart <explorer.exe> and restore tabs
 [Author]
   boredwz | https://github.com/boredwz
 
-[URL]
-  Gist | https://gist.github.com/boredwz/1511cb7f81f0c01f3373a3d2ad8a7e9d
-
-[Info]
-  Tested on:
-  - Windows 10 (build 19045.4780)
-  - PowerShell v5.1
-
-  🔔 I don't know if it is possible to open a single window with multiple tabs on Windows 11.
+[Description]
+  Tested:
+    - Windows 10 (build 19045.4780)
+    - PowerShell v5.1
   
-  Restart <explorer.exe> and restore tabs:
-    1. Respectively (save tab state)
-      Normal -> Normal
-      Maximized -> Maximized
-      Minimized -> Normalized and Minimized
-    2. Minimized
-      Any -> Minimized
+  <!>
+    It is not possible to open a single explorer window with multiple tabs on Windows 11.
+    But you can use this tool as a workaround:
+    https://github.com/w4po/ExplorerTabUtility
   
 [One-liner] Respectively:
-  $e='explorer';$t=@();(New-Object -co Shell.Application).Windows()|%{$t+=@{p=$_.LocationURL;w=if($_.Top -lt 0){if($_.Top -lt -8000){'min'}else{'max'}}else{'nor'}}};kill -n $e -for;sleep 1;if(!($null=ps $e -ea 0)){saps $e};sleep 4;$t|%{saps $_.p -win $_.w}
+  $e='explorer';$t=@();(New-Object -co Shell.Application).Windows()|%{$t+=@{p=$_.Document.Folder.Self.Path;w=if($_.Top -lt 0){if($_.Top -lt -8000){'min'}else{'max'}}else{'nor'}}};kill -n $e -for;sleep 1;if(!($null=ps $e -ea 0)){saps $e};sleep 5;$t|%{sleep -m 500;saps $_.p -win $_.w}
 
 [One-liner] Minimized:
-  $e='explorer';$t=@();(New-Object -co Shell.Application).Windows()|%{$t+=$_.LocationURL};kill -n $e -for;sleep 1;if(!($null=ps $e -ea 0)){saps $e};sleep 4;$t|%{saps $_ -win min}
+  $e='explorer';$t=@();(New-Object -co Shell.Application).Windows()|%{$t+=$_.Document.Folder.Self.Path};kill -n $e -for;sleep 1;if(!($null=ps $e -ea 0)){saps $e};sleep 5;$t|%{sleep -m 500;saps $_ -win min}
 
 .PARAMETER Minimize
 Any window state -> Minimized.
-Aliases: "Min", "Minimize", "Minimized".
+Aliases: [-M], [-Min], [-Minimize], [-Minimized].
 
 .EXAMPLE
 PS C:\> & ".\RestartExplorer.ps1"
@@ -46,7 +38,7 @@ PS C:\> & ".\RestartExplorer.ps1" -minimized
 #>
 
 param(
-    [Alias("min","minimized")][switch]$Minimize
+    [Alias("m","min","minimized")][switch]$Minimize
 )
 
 # Get explorer windows
@@ -56,7 +48,7 @@ $explorerWindows = @()
     if($_.Top -lt 0) {$windowState = 'Maximized'}
     if($_.Top -lt -8000 -or $Minimize) {$windowState = 'Minimized'}
     $explorerWindows += @{
-        Location=$_.LocationURL;
+        Location=$_.Document.Folder.Self.Path;
         WindowState=$windowState
     }
 }
@@ -65,9 +57,10 @@ $explorerWindows = @()
 Stop-Process -ProcessName explorer -Force
 Start-Sleep 1
 if (!($null = Get-Process explorer -ErrorAction SilentlyContinue)) {Start-Process explorer}
-Start-Sleep 4
+Start-Sleep -m 5000
 
 # Open saved explorer windows
 $explorerWindows | ForEach-Object {
+    Start-Sleep -m 500
     Start-Process $_.Location -WindowStyle $_.WindowState
 }
